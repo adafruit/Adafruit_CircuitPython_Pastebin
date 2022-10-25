4f99632cc2a19bccf89a8faf32ec6a1f49f0dda8
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-10-24 20:30:18 -0400

    Initial commit

---
+++ adafruit_pastebin/__init__.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 Alec Delaney for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_pastebin`
+===================
+
+CircuitPython library for interacting with online pastebin services
+
+
+* Author(s): Alec Delaney
+
+Implementation Notes
+--------------------
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Pastebin.git"
+
+
---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/pastebin_simpletest.py
+    :caption: examples/pastebin_simpletest.py
+    :linenos:
