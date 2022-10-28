55506a76ca86c4bf616f281ddcb6299d0dbf9f9c
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-10-28 18:29:51 -0400

    Fix microcontroller example

--- examples/pastebin_simpletest.py
+++ examples/pastebin_simpletest.py
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: Unlicense
 
 import ssl
-import socket
+import wifi
+import socketpool
 import adafruit_requests as requests
 from adafruit_pastebin.pastebin import PasteBin, ExpirationSetting, PrivacySetting
 
@@ -15,7 +16,7 @@ except ImportError:
 
 wifi.radio.connect(secrets["ssid"], secrets["password"])
 pool = socketpool.SocketPool(wifi.radio)
-session = adafruit_requests.Session(pool, ssl.create_default_context())
+session = requests.Session(pool, ssl.create_default_context())
 
 auth_key = secrets["auth_key"]
 
